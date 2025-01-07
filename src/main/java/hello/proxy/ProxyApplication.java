1c48efef29885db64d2f8e7300116b98cfa9db7f
legoChoi <chlx159@gmail.com>
2025-01-07 23:42:28 +0900

    v1 - 인터페이스와 구현 클래스 - 스프링 빈으로 수동 등록

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,12 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") // 주의: 컴포넌트 스캔 위치 지정
 public class ProxyApplication {
 
 	public static void main(String[] args) {
