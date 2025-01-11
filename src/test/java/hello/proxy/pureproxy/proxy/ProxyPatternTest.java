e233ddc26d9a1de3c18ff986bc6976fa3d89f502
legoChoi <chlx159@gmail.com>
2025-01-11 17:34:31 +0900

    프록시 패턴 - 예제 2

--- src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,17 @@ public class ProxyPatternTest {
         client.execute(); // 1초
 
         // 총 3초
+    }
+
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject); // 프록시가 중간 브로커 역할
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+
+        client.execute(); // 실제 객체 호출 O
+        client.execute(); // 실제 객체 호출 X
+        client.execute(); // 실제 객체 호출 X
     }
 }
